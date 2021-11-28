4ec09e75ab0f4a5c04ed7f83a0b158228fefc26f
OriginalPou <chaari.mahdi@yahoo.fr>
2021-11-28 13:23:57 +0100

    print method added

--- src/NES/NES_Algorithms.java
+++ src/NES/NES_Algorithms.java
@@ -3,9 +3,13 @@ package NES;
 
 public class NES_Algorithms {
 	
+	private static int nbop = 0;
+	
 
 	public static int sumR_M(int number, int[] coins, int[] D) {
 			
+			nbop++;
+		
 			if (number<0) {
 				
 				return(0);
@@ -28,6 +32,8 @@ public class NES_Algorithms {
 	
 	public static int sumR(int number,int[] coins) {
 			
+			nbop++;
+			
 			if (number<0) {	
 				return(0);	
 			}
@@ -43,28 +49,57 @@ public class NES_Algorithms {
 			}
 		}
 	
-	public static int sumR_M_Envolope(Object[] data) throws Exception {
+	public static Object[] sumR_M_Envolope(Object[] data) throws Exception {
+		
 		if (data.length!=3) {
 			throw new Exception("Wrong Input");
 		}
 		
+		nbop=0;
+		
 		int number = (int) data[0];
 		int[] coins= (int[]) data[1];
 		int[] D = (int[]) data[2];
-
 		
-		return(sumR_M(number,coins,D));
+		Object[] res = new Object [3];
+		
+		res[0]=number;
+		
+		long t0 = System.currentTimeMillis();
+		sumR_M(number,coins,D);
+		long time= System.currentTimeMillis() - t0;
+		
+		res[1]=nbop;
+		res[2]=(int)time;
+		
+		return (res);
+		
+		
 	}
 	
-	public static int sumR_Envolope(Object[] data) throws Exception {
+	
+	
+	public static Object[] sumR_Envolope(Object[] data) throws Exception {
 		if (data.length!=2) {
 			throw new Exception("Wrong Input");
 		}
 		
+		nbop=0;
+		
 		int number = (int) data[0];
 		int[] coins= (int[]) data[1];
+		
+		Object[] res = new Object [3];
+		
+		long t0 = System.currentTimeMillis();
+		sumR(number,coins);
+		long time= System.currentTimeMillis() - t0;
+		
+		res[1]=nbop;
+		res[2]=(int)time;
+		
+		return (res);
 
 		
-		return(sumR(number,coins));
 	}
 }
--- src/TP_Algo/AlgorithmTester.java
+++ src/TP_Algo/AlgorithmTester.java
@@ -3,6 +3,9 @@ package TP_Algo;
 import NES.NES_DataGenerator;
 import NES.NES_Algorithms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -12,41 +15,54 @@ public class AlgorithmTester {
 	
 
 	public static void main(String[] args) throws Exception {
-		
-		Method sumR_M_Envolope = NES_Algorithms.class.getMethod("sumR_M_Envolope", Object[].class);
+		/*
 		System.out.println(sumR_M_Envolope);
 		Constructor constructor = NES_DataGenerator.class.getConstructor(int.class);
 		System.out.println(constructor);
+		*/
 		
-		System.out.println(testAlgorithm(NES_DataGenerator.class, 0, sumR_M_Envolope));
+		Method sumR_M_Envolope = NES_Algorithms.class.getMethod("sumR_M_Envolope", Object[].class);
+		
+		Object[] res =testAlgorithm(NES_DataGenerator.class, 0, sumR_M_Envolope);
+		
+		print(res, "BOOM/"+ res[0]+ ".csv");
 		
 		
 	}
 	
-	public static  int testAlgorithm (Class DataGenerator,int complexity, Method algorithm) throws Exception, IllegalArgumentException, InvocationTargetException {
+	public static  Object[] testAlgorithm (Class DataGenerator,int complexity, Method algorithm) throws Exception, IllegalArgumentException, InvocationTargetException {
 		
 		Constructor constructor = DataGenerator.getConstructor(int.class);
 		DataGenerator data =(DataGenerator) constructor.newInstance(complexity);;
 		Object[] data_generated = data.generateData();
-		
-		System.out.println(data_generated[0]);
-		int[] coins = (int[])data_generated[1];
-		System.out.println(coins.length);
-		for (int i=0; i<coins.length;i++) {
-			System.out.println(coins[i]);
-		}
 		
 		int index=1;
 		
 		while (true) {
 			try {
 				Object[] needed_data = Arrays.copyOfRange(data_generated, 0, index);
-				return (int) (algorithm.invoke(null, new Object[]{needed_data}));
+				return (Object[]) (algorithm.invoke(null, new Object[]{needed_data}));
 			}
 			catch (Exception e) {
-				index++;
+				if(index < data_generated.length) {
+					index++;
+				}else {
+					throw new Exception("Algorithm does not match data generator");
+				}
+					
 			}
 		}		
+	}
+	
+	private static void print(Object[]res, String filename) throws FileNotFoundException {
+		File csvFile = new File(filename);
+		PrintWriter out = new PrintWriter(csvFile);
+		out.print((int)res[0]);
+		out.print(",");
+		out.print((int)res[1]);
+		out.print(",");
+		out.println((int)res[2]);
+		out.close();
 	}
 	
 }
